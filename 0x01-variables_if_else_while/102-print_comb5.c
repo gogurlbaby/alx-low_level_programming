1b83efec34724c9dcf8f119f4bf9feddfcd396ee
gogurlbaby <philiahammmond@gmail.com>
2022-04-08 15:25:53 -0700

    Tried again

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -8,19 +10,48 @@
  */
 int main(void)
 {
-	int d;
+int i, j, k, l;
 
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-
-	return (0);
+i = j = k = 48; l = 49;
+while  ((i < 58))
+{
+putchar(i); 
+putchar(j); 
+putchar(32); 
+putchar(k); 
+putchar(l);
+if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+{
+putchar('\n'); i++;
+}
+else
+{
+putchar(44); 
+putchar(32);
+if ((k == 57) && (l == 57))
+{
+if (j < 56)
+{
+l = ++j + 1; k = i;
+}
+else if (j == 56)
+{
+j++; k = i + 1; l = 48;
+}
+else if (j == 57)
+{
+j = 48; l = 49; k = ++i;
+}
+}
+else if (l < 57)
+{
+l++;
+}
+else
+{
+l = 48; k++;
+}
+}
+}
+return (0);
 }
