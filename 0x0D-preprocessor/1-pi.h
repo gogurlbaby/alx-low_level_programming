71cf43a0a91ea9a8137f70db52ec25ca79f22e18
gogurlbaby <philiahammmond@gmail.com>
2022-05-14 07:43:13 -0700

    1-pi.h file

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+typedef int make_iso_compilers_happy;
+
+#endif
